0dfeb077c43a69084b819a82d8d83780d58452d5
Craig Cuthbertson <1002386c@student.gla.ac.uk>
2012-12-06 15:38:04 +0000

    Test: Git

--- src/thesaurus/gui/window/Driver.java
+++ src/thesaurus/gui/window/Driver.java
@@ -6,7 +6,7 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		MainWindow currentWindow = new MainWindow();
-		
+		System.out.println("Marc's cool. Matthews team bitch. fact.");
 		currentWindow.launchProgram(args);
 
 	}
