922e0f3488aa331eb1fcd4596c7c2cd29c373dc8
Capavanni <1000011c@student.gla.ac.uk>
2012-12-07 16:11:51 +0000

    Partially set up controller class

--- src/thesaurus/gui/window/SplashRoot.java
+++ src/thesaurus/gui/window/SplashRoot.java
@@ -1,12 +1,11 @@
 package thesaurus.gui.window;
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
-
+import thesaurus.controller.*;
 import thesaurus.gui.window.VisualisationRoot;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -22,12 +21,18 @@ import javafx.stage.FileChooser;
 public class SplashRoot extends AnchorPane {
 
 	private MainWindow referenceWindow;
+	
+	private SystemController currentController;
 
 	public SplashRoot(MainWindow inputWindow) {
+		
+		referenceWindow = inputWindow;
+		
+		currentController = new SystemController(referenceWindow);
 
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/resourcePackage/splashLayout.fxml"));
 		fxmlLoader.setRoot(this);
-		fxmlLoader.setController(this);
+		fxmlLoader.setController(currentController);
 		
 		try {
 			fxmlLoader.load();
@@ -35,63 +40,6 @@ public class SplashRoot extends AnchorPane {
 			throw new RuntimeException(exception);
 		}
 		
-		referenceWindow = inputWindow;
-		
-	}
-
-	private void saveFile(String content, File file) {
-		try {
-			FileWriter fileWriter = null;
-			fileWriter = new FileWriter(file);
-			fileWriter.write(content);
-			fileWriter.close();
-		} catch (IOException ex) {}
-
-	}
-
-	@FXML
-	protected void doCreate() throws IOException {
-		
-		FileChooser fileChooser = new FileChooser();
-		FileChooser.ExtensionFilter graphmlFilter = new FileChooser.ExtensionFilter("GraphML files (*.graphml)", "*.graphml");
-		FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
-		fileChooser.getExtensionFilters().add(graphmlFilter);
-		fileChooser.getExtensionFilters().add(xmlFilter);
-		File file = fileChooser.showSaveDialog(referenceWindow.getStage());
-		if (file != null) {
-			saveFile("test", file);
-		}
-
-		VisualisationRoot visualisationRootCurrent = new VisualisationRoot(referenceWindow);
-		referenceWindow.getStage().setScene(new Scene(visualisationRootCurrent, 800, 600));
-	}
-
-	@FXML
-	protected void doImport() throws IOException {
-		
-		FileChooser fileChooser = new FileChooser();
-		FileChooser.ExtensionFilter graphmlFilter = new FileChooser.ExtensionFilter("GraphML files (*.graphml)", "*.graphml");
-		FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
-		FileChooser.ExtensionFilter allFilter = new FileChooser.ExtensionFilter("All files", "*");
-		fileChooser.getExtensionFilters().add(graphmlFilter);
-		fileChooser.getExtensionFilters().add(xmlFilter);
-		fileChooser.getExtensionFilters().add(allFilter);
-		
-		File file = fileChooser.showOpenDialog(referenceWindow.getStage());
-		if (file != null){
-			Scanner fileScanner = null;
-			try {
-				fileScanner = new Scanner(file);
-			} catch (FileNotFoundException e1) {
-				System.out.println();
-			}
-			if (fileScanner != null) {
-				//System.out.println(fileScanner.next());
-			}
-		}
-		
-		VisualisationRoot visualisationRootCurrent = new VisualisationRoot(referenceWindow);		
-		referenceWindow.getStage().setScene(new Scene(visualisationRootCurrent, 800, 600));
 	}
 
 }
