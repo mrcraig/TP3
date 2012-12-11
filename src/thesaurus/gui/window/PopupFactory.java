ce8cb613e4673e1a04ee43140c7d1d0f2f29fb6c
Capavanni <1000011c@student.gla.ac.uk>
2012-12-11 16:33:22 +0000

    Further upgrades to popup boxes, a lot of work backend not frontend

---
+++ src/thesaurus/gui/window/PopupFactory.java
@@ -0,0 +1,158 @@
+package thesaurus.gui.window;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Popup;
+
+
+public class PopupFactory {
+	
+	Popup currentPopup = null;
+	
+	public PopupFactory(String inputChoice){
+		if (inputChoice.equals("add")) {
+			currentPopup = new Popup();
+			currentPopup.getContent().add(makeCanvasAdd());
+		} else if (inputChoice.equals("edit")) {
+			currentPopup = new Popup();
+			currentPopup.getContent().add(makeCanvasEdit());
+		} else if (inputChoice.equals("remove")) {
+			currentPopup = new Popup();
+			currentPopup.getContent().add(makeCanvasRemove());
+		}
+	}
+	
+	private Pane makeCanvasAdd() {
+		Pane canvas = new Pane();
+		canvas.setPrefSize(200, 200);
+		Text addWordLabel = new Text();
+		addWordLabel.relocate(35, 10);
+		addWordLabel.setText("Add Word");
+		addWordLabel.setScaleX(2);
+		addWordLabel.setScaleY(2);
+		Text promptWordLabel = new Text();
+		promptWordLabel.relocate(10, 52);
+		promptWordLabel.setText("Word: ");
+		TextField addWordInput = new TextField();
+		addWordInput.setPrefWidth(120);
+		addWordInput.relocate(70, 50);
+		Text promptSynLabel = new Text();
+		promptSynLabel.relocate(10, 82);
+		promptSynLabel.setText("Synonyms: ");
+		TextField addSynInput = new TextField();
+		addSynInput.setPrefWidth(120);
+		addSynInput.relocate(70, 80);
+		Text promptAntLabel = new Text();
+		promptAntLabel.relocate(10, 112);
+		promptAntLabel.setText("Antonyms: ");
+		TextField addAntInput = new TextField();
+		addAntInput.setPrefWidth(120);
+		addAntInput.relocate(70, 110);
+		Button confirmButton = new Button();
+		confirmButton.setText("Confirm");
+		confirmButton.relocate(30, 160);
+		Button cancelButton = new Button();
+		cancelButton.setText("Cancel");
+		cancelButton.relocate(100, 160);
+		cancelButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				currentPopup.hide();
+			}
+		});
+		canvas.getChildren().addAll(addWordLabel, promptWordLabel,
+				addWordInput, promptSynLabel, addSynInput, promptAntLabel,
+				addAntInput, confirmButton, cancelButton);
+		canvas.setStyle("	-fx-background-color: #dfdfdf;"
+				+ "-fx-border-color: black;" + "-fx-border-width: 1px;");
+		return canvas;
+	}
+	
+	private Pane makeCanvasEdit() {
+		Pane canvas = new Pane();
+		canvas.setPrefSize(200, 200);
+		Text addWordLabel = new Text();
+		addWordLabel.relocate(35, 10);
+		addWordLabel.setText("Edit Word");
+		addWordLabel.setScaleX(2);
+		addWordLabel.setScaleY(2);
+		Text promptWordLabel = new Text();
+		promptWordLabel.relocate(10, 52);
+		promptWordLabel.setText("Word: ");
+		TextField addWordInput = new TextField();
+		addWordInput.setPrefWidth(120);
+		addWordInput.relocate(70, 50);
+		Text promptSynLabel = new Text();
+		promptSynLabel.relocate(10, 82);
+		promptSynLabel.setText("Synonyms: ");
+		TextField addSynInput = new TextField();
+		addSynInput.setPrefWidth(120);
+		addSynInput.relocate(70, 80);
+		Text promptAntLabel = new Text();
+		promptAntLabel.relocate(10, 112);
+		promptAntLabel.setText("Antonyms: ");
+		TextField addAntInput = new TextField();
+		addAntInput.setPrefWidth(120);
+		addAntInput.relocate(70, 110);
+		Button confirmButton = new Button();
+		confirmButton.setText("Confirm");
+		confirmButton.relocate(30, 160);
+		Button cancelButton = new Button();
+		cancelButton.setText("Cancel");
+		cancelButton.relocate(100, 160);
+		cancelButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				currentPopup.hide();
+			}
+		});
+		canvas.getChildren().addAll(addWordLabel, promptWordLabel,
+				addWordInput, promptSynLabel, addSynInput, promptAntLabel,
+				addAntInput, confirmButton, cancelButton);
+		canvas.setStyle("	-fx-background-color: #dfdfdf;"
+				+ "-fx-border-color: black;" + "-fx-border-width: 1px;");
+		return canvas;
+	}
+	
+	private Pane makeCanvasRemove() {
+		Pane canvas = new Pane();
+		canvas.setPrefSize(200, 200);
+		Text addWordLabel = new Text();
+		addWordLabel.relocate(45, 10);
+		addWordLabel.setText("Remove Word");
+		addWordLabel.setScaleX(2);
+		addWordLabel.setScaleY(2);
+		Text promptWordLabel = new Text();
+		promptWordLabel.relocate(10, 52);
+		promptWordLabel.setText("Word: ");
+		TextField addWordInput = new TextField();
+		addWordInput.setPrefWidth(120);
+		addWordInput.relocate(70, 50);
+		Button confirmButton = new Button();
+		confirmButton.setText("Confirm");
+		confirmButton.relocate(30, 160);
+		Button cancelButton = new Button();
+		cancelButton.setText("Cancel");
+		cancelButton.relocate(100, 160);
+		cancelButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				currentPopup.hide();
+			}
+		});
+		canvas.getChildren().addAll(addWordLabel, promptWordLabel,
+				addWordInput, confirmButton, cancelButton);
+		canvas.setStyle("	-fx-background-color: #dfdfdf;"
+				+ "-fx-border-color: black;" + "-fx-border-width: 1px;");
+		return canvas;
+	}
+
+	public Popup getPopup() {
+		return currentPopup;
+	}
+
+}
--- src/thesaurus/gui/window/VisualisationRoot.java
+++ src/thesaurus/gui/window/VisualisationRoot.java
@@ -3,19 +3,13 @@ package thesaurus.gui.window;
 import java.io.File;
 import java.io.IOException;
 import java.util.LinkedList;
-
-import thesaurus.parser.*;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.Text;
 import javafx.stage.Popup;
 import thesaurus.controller.SystemController;
 import thesaurus.gui.canvas.ViewGraph;
+import thesaurus.parser.Parser;
+import thesaurus.parser.Vertex;
 
 /**
  * This class is an extension of AnchorPane and defines how the visualisation
@@ -85,156 +79,10 @@ public class VisualisationRoot extends AnchorPane {
 	}
 
 	public void showPopup(String inputChoice) {
-		if (inputChoice.equals("add")) {
-			showPopupAdd();
-		} else if (inputChoice.equals("edit")) {
-			showPopupEdit();
-		} else if (inputChoice.equals("remove")) {
-			showPopupRemove();
-		}
-	}
-
-	private void showPopupAdd() {
-		Popup popup = new Popup();
-		popup.getContent().addAll(makeCanvasAdd(popup));
-		popup.show(referenceWindow.getStage());
-	}
-	
-	private void showPopupEdit() {
-		Popup popup = new Popup();
-		popup.getContent().addAll(makeCanvasEdit(popup));
-		popup.show(referenceWindow.getStage());
-	}
-
-	private void showPopupRemove() {
-		Popup popup = new Popup();
-		popup.getContent().addAll(makeCanvasRemove(popup));
-		popup.show(referenceWindow.getStage());
-	}
-
-	private Pane makeCanvasAdd(final Popup inputPopup) {
-		Pane canvas = new Pane();
-		canvas.setPrefSize(200, 200);
-		Text addWordLabel = new Text();
-		addWordLabel.relocate(35, 10);
-		addWordLabel.setText("Add Word");
-		addWordLabel.setScaleX(2);
-		addWordLabel.setScaleY(2);
-		Text promptWordLabel = new Text();
-		promptWordLabel.relocate(10, 52);
-		promptWordLabel.setText("Word: ");
-		TextField addWordInput = new TextField();
-		addWordInput.setPrefWidth(120);
-		addWordInput.relocate(70, 50);
-		Text promptSynLabel = new Text();
-		promptSynLabel.relocate(10, 82);
-		promptSynLabel.setText("Synonyms: ");
-		TextField addSynInput = new TextField();
-		addSynInput.setPrefWidth(120);
-		addSynInput.relocate(70, 80);
-		Text promptAntLabel = new Text();
-		promptAntLabel.relocate(10, 112);
-		promptAntLabel.setText("Antonyms: ");
-		TextField addAntInput = new TextField();
-		addAntInput.setPrefWidth(120);
-		addAntInput.relocate(70, 110);
-		Button confirmButton = new Button();
-		confirmButton.setText("Confirm");
-		confirmButton.relocate(30, 160);
-		Button cancelButton = new Button();
-		cancelButton.setText("Cancel");
-		cancelButton.relocate(100, 160);
-		cancelButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				inputPopup.hide();
-			}
-		});
-		canvas.getChildren().addAll(addWordLabel, promptWordLabel,
-				addWordInput, promptSynLabel, addSynInput, promptAntLabel,
-				addAntInput, confirmButton, cancelButton);
-		canvas.setStyle("	-fx-background-color: #dfdfdf;"
-				+ "-fx-border-color: black;" + "-fx-border-width: 1px;");
-		return canvas;
-	}
-	
-	private Pane makeCanvasEdit(final Popup inputPopup) {
-		Pane canvas = new Pane();
-		canvas.setPrefSize(200, 200);
-		Text addWordLabel = new Text();
-		addWordLabel.relocate(35, 10);
-		addWordLabel.setText("Edit Word");
-		addWordLabel.setScaleX(2);
-		addWordLabel.setScaleY(2);
-		Text promptWordLabel = new Text();
-		promptWordLabel.relocate(10, 52);
-		promptWordLabel.setText("Word: ");
-		TextField addWordInput = new TextField();
-		addWordInput.setPrefWidth(120);
-		addWordInput.relocate(70, 50);
-		Text promptSynLabel = new Text();
-		promptSynLabel.relocate(10, 82);
-		promptSynLabel.setText("Synonyms: ");
-		TextField addSynInput = new TextField();
-		addSynInput.setPrefWidth(120);
-		addSynInput.relocate(70, 80);
-		Text promptAntLabel = new Text();
-		promptAntLabel.relocate(10, 112);
-		promptAntLabel.setText("Antonyms: ");
-		TextField addAntInput = new TextField();
-		addAntInput.setPrefWidth(120);
-		addAntInput.relocate(70, 110);
-		Button confirmButton = new Button();
-		confirmButton.setText("Confirm");
-		confirmButton.relocate(30, 160);
-		Button cancelButton = new Button();
-		cancelButton.setText("Cancel");
-		cancelButton.relocate(100, 160);
-		cancelButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				inputPopup.hide();
-			}
-		});
-		canvas.getChildren().addAll(addWordLabel, promptWordLabel,
-				addWordInput, promptSynLabel, addSynInput, promptAntLabel,
-				addAntInput, confirmButton, cancelButton);
-		canvas.setStyle("	-fx-background-color: #dfdfdf;"
-				+ "-fx-border-color: black;" + "-fx-border-width: 1px;");
-		return canvas;
-	}
-	
-	private Pane makeCanvasRemove(final Popup inputPopup) {
-		Pane canvas = new Pane();
-		canvas.setPrefSize(200, 200);
-		Text addWordLabel = new Text();
-		addWordLabel.relocate(45, 10);
-		addWordLabel.setText("Remove Word");
-		addWordLabel.setScaleX(2);
-		addWordLabel.setScaleY(2);
-		Text promptWordLabel = new Text();
-		promptWordLabel.relocate(10, 52);
-		promptWordLabel.setText("Word: ");
-		TextField addWordInput = new TextField();
-		addWordInput.setPrefWidth(120);
-		addWordInput.relocate(70, 50);
-		Button confirmButton = new Button();
-		confirmButton.setText("Confirm");
-		confirmButton.relocate(30, 160);
-		Button cancelButton = new Button();
-		cancelButton.setText("Cancel");
-		cancelButton.relocate(100, 160);
-		cancelButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				inputPopup.hide();
-			}
-		});
-		canvas.getChildren().addAll(addWordLabel, promptWordLabel,
-				addWordInput, confirmButton, cancelButton);
-		canvas.setStyle("	-fx-background-color: #dfdfdf;"
-				+ "-fx-border-color: black;" + "-fx-border-width: 1px;");
-		return canvas;
+		PopupFactory currentPopupFactory = new PopupFactory(inputChoice);
+		Popup currentPopup = currentPopupFactory.getPopup();
+		currentPopup.show(referenceWindow.getStage());
+		currentPopup.setY(currentPopup.getY()+10);
 	}
 
 }
