9b33c2e51f92da3187d7c6799a23bf6f183cda76
JamesWKerr <0805089k@student.gla.ac.uk>
2012-12-12 15:25:05 +0000

    I hate git

--- src/thesaurus/parser/Driver.java
+++ src/thesaurus/parser/Driver.java
@@ -1,45 +1,11 @@
 package thesaurus.parser;
 
-
-
-
-
-/*
- * need to parse vertices first before edge
- */
-
-
 public class Driver 
 {
-	public static void main(String args[]){
-		
+
+	public static void main(String[] args)
+	{
 		XmlRead read = new XmlRead("/resourcePackage/testAgain.xml");
-		//Parser read = new Parser("/resourcePackage/data.xml");
-		XmlWrite add = new XmlWrite("/resourcePackage/testAgain.xml");
-		
-		//System.out.println(read.getSynmsForOne("Happy"));
-		
-		
-		/*
-		Vertex blue = new Vertex("0");
-		Vertex red = new Vertex("1");
-		Vertex green = new Vertex("2");
-		
-		blue.setWord("blue");
-		red.setWord("red");
-		green.setWord("green");
-		
-		blue.addToAdjList(blue);
-		red.addToAdjList(blue); red.addToAdjList(green);
-		green.addToAdjList(green); green.addToAdjList(blue); green.addToAdjList(red);
-		
-		add.addVertex(blue);
-		add.addVertex(red);
-		add.addVertex(green);  */
-		
-		
-		//need to get all vertices before edges?
-		System.out.println(read.getAllNodes());	
-			
-		}
+		System.out.println(read.getSynmsFor("Happy"));
+	}
 }
