f319266df5c50a15e8f9ef955953f0e8614b0494
JamesWKerr <0805089k@student.gla.ac.uk>
2012-12-12 16:46:55 +0000

    constructor chaos

--- src/thesaurus/parser/Driver.java
+++ src/thesaurus/parser/Driver.java
@@ -5,6 +5,7 @@ public class Driver
 
 	public static void main(String[] args)
 	{
+		//File test = new File()
 		XmlRead read = new XmlRead(null);
 		System.out.println(read.getSynmsForOne("Peaceful"));
 		
