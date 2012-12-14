9e64f05ccf5b55f2211531f548b08ce6b949c663
Atoshum <1007789c@student.gla.ac.uk>
2012-12-14 11:38:07 +0000

    A test vertices are updated

--- src/thesaurus/spring/myMian.java
+++ src/thesaurus/spring/myMian.java
@@ -18,90 +18,109 @@ public class myMian extends Application {
 	int len = 1000;
 	static Vertex myW;
 
-	FrSpring2 fr;
+	static FrSpring2 fr;
 
 	public static void main(String[] args) {
-
+		mystart();
 		launch(args);
 	}
 
-	public void mystart() {
+	public static void mystart() {
 		// TODO Auto-generated method stub
-		Vertex v1 = new Vertex("big");
-		this.myW = v1;
-		Vertex v2 = new Vertex("adult");
-		Vertex v3 = new Vertex("elder");
-		Vertex v4 = new Vertex("full-grown");
-		Vertex v5 = new Vertex("grownup");
-		Vertex v6 = new Vertex("mature");
-		Vertex v7 = new Vertex("tall");
-		Vertex v8 = new Vertex("complete");
-		Vertex v9 = new Vertex("full-grown");
-		Vertex v10 = new Vertex("fit");
-		Vertex v11 = new Vertex("developed");
-		Vertex v12 = new Vertex("grown");
-		Vertex v13 = new Vertex("of age");
-		Vertex v14 = new Vertex("ripe");
-		Vertex v15 = new Vertex("ancient");
-		Vertex v16 = new Vertex("full grown");
-		Vertex v17 = new Vertex("first-born");
-		Vertex v18 = new Vertex("more mature");
-		Vertex v19 = new Vertex("older");
-		Vertex v20 = new Vertex("senior");
-		Vertex v21 = new Vertex("settle");
-
+		Vertex v1 = new Vertex("happy");
+		//this.myW = v1;
+		Vertex v2 = new Vertex("joyfull");
+		Vertex v3 = new Vertex("cheerfull");
+		Vertex v4 = new Vertex("jubilant");
+		Vertex v5 = new Vertex("merry");
+		Vertex v6 = new Vertex("upbeat");
+		Vertex v7 = new Vertex("peacefull");
+		Vertex v8 = new Vertex("friendly");
+		Vertex v9 = new Vertex("affable");
+		Vertex v10 = new Vertex("affectionate");
+		Vertex v11 = new Vertex("amiable");
+		Vertex v12 = new Vertex("mature");
+		Vertex v13 = new Vertex("fit");
+		Vertex v14 = new Vertex("full grown");
+		Vertex v15 = new Vertex("settle");
+		
 		v1.addToAdjList(v2);
 		v1.addToAdjList(v3);
-		// v1.addToAdjList(v4);
-		v1.addToAdjList(v5);
-		v1.addToAdjList(v6);
-		v1.addToAdjList(v7);
-		v1.addToAdjList(v9);
+	    v1.addToAdjList(v6);
+    	v1.addToAdjList(v7);
+		v1.addToAdjList(v8);
+     	
 
-		v2.addToAdjList(v11);
-		v2.addToAdjList(v12);
-		v2.addToAdjList(v10);
-		v2.addToAdjList(v13);
-		v2.addToAdjList(v14);
-		v2.addToAdjList(v14);
+		
+		v2.addToAdjList(v1);
+		v2.addToAdjList(v4);
+		v2.addToAdjList(v5);
+		v2.addToAdjList(v6);
+//		v2.addToAdjList(v14);
 		//
-//		v3.addToAdjList(v5);
+         v3.addToAdjList(v5);
 		 //v3.addToAdjList(v11);
-		 v3.addToAdjList(v12);
-		 v3.addToAdjList(v13);
-		 v3.addToAdjList(v14);
-		v3.addToAdjList(v6);
-		v3.addToAdjList(v15);
-		v3.addToAdjList(v17);
-		v3.addToAdjList(v18);
-		v3.addToAdjList(v19);
-		v3.addToAdjList(v20);
+		
+		 v3.addToAdjList(v1);
+		 v3.addToAdjList(v6);
+//		v3.addToAdjList(v6);
+//		v3.addToAdjList(v15);
+//		v3.addToAdjList(v17);
+//		v3.addToAdjList(v18);
+//		v3.addToAdjList(v19);
+//		v3.addToAdjList(v20);
 
-		v4.addToAdjList(v8);
-		v4.addToAdjList(v10);
-		v4.addToAdjList(v11);
+		v4.addToAdjList(v1);
+		v4.addToAdjList(v2);
+		
 
+		v5.addToAdjList(v1);
 		v5.addToAdjList(v2);
-		v5.addToAdjList(v4);
-
-		v6.addToAdjList(v8);
-		v6.addToAdjList(v10);
-		v6.addToAdjList(v16);
-		v6.addToAdjList(v21);
-
-		fr = new FrSpring2(v1);
+		v5.addToAdjList(v3);
+		//v5.addToAdjList(v6);
+		
+		v6.addToAdjList(v1);
+		v6.addToAdjList(v2);
+		v6.addToAdjList(v3);
+		//v6.addToAdjList(v5);
+		
+		v7.addToAdjList(v1);
+		v7.addToAdjList(v8);
+		
+		v8.addToAdjList(v1);
+		v8.addToAdjList(v7);
+		v8.addToAdjList(v9);
+		v8.addToAdjList(v10);
+		v8.addToAdjList(v11);
+		
+		v9.addToAdjList(v10);
+		v9.addToAdjList(v11);
+		
+		v12.addToAdjList(v13);
+		v12.addToAdjList(v14);
+		v12.addToAdjList(v15);
+		//v5.addToAdjList(v6);
+		
+		fr = new FrSpring2(v2);
 		// fr.getPositions();
 		// fr.test();
 
 	}
 
 	public void start(Stage primaryStage) {
-		mystart();
+		
 		Group root = new Group();
+		
 		graph = new Canvas(wid, len);
 		final GraphicsContext gc = graph.getGraphicsContext2D();
 		// drawShapes(gc, myW);
-
+	//	while ((((double)10000 - fr.temprature) < 5)&& (((double)10000 - fr.temprature) >= 0) ){
+		root.getChildren().add(graph);
+		primaryStage.setScene(new Scene(root));
+		
+		primaryStage.show();
+		
+		
 		for (Vertex x : fr.getVertices()) {
 			double sX = x.getPos().getX();
 			double sY = x.getPos().getY();
@@ -116,23 +135,15 @@ public class myMian extends Application {
 		}
 
 		for (Vertex v : fr.getVertices()) {
+			mywait (1);
 			drawShapes(gc, v);
 		}
-		// for (Vertex v : fr.getVertices() ){
-		// for (Vertex x: v.getAdjList()){
-		// if (gc == null)System.out.println("I am null");
-		// if ((x != null)|| (v != null)){
-		// drawShapes1(gc,v,x);}
-		// }
-		// }
-		// TODO Auto-generated method stub
-		root.getChildren().add(graph);
-		primaryStage.setScene(new Scene(root));
-		primaryStage.show();
-
+		
+		
+		
 	}
 
-	private static void drawShapes(GraphicsContext gc, Vertex v) {
+	public void drawShapes(GraphicsContext gc, Vertex v) {
 		gc.setStroke(Color.RED);
 		gc.setFill(Color.WHITE);
 		gc.strokeOval((v.getPos().getX() - 37), (v.getPos().getY() - 13), 74,
@@ -146,7 +157,7 @@ public class myMian extends Application {
 																					// connector
 		gc.setFill(Color.BLACK);
 		gc.setFont(new Font(14));
-		gc.fillText(v.getWord(), (v.getPos().getX() - 25),
+		gc.fillText(v.getIndex(), (v.getPos().getX() - 25),
 				(v.getPos().getY() + 10));
 		// if (v.equals(myW)) {
 		// gc.fillRect(v.getPos().getX(), v.getPos().getY(), 20, 20);
@@ -155,7 +166,14 @@ public class myMian extends Application {
 		// }
 
 	}
-
+	public static void mywait (int k){
+		long time0, time1;
+		time0 = System.currentTimeMillis();
+		do{
+		time1 = System.currentTimeMillis();
+		}
+		while ((time1 - time0) < k * 100);
+		}
 	private static void drawShapes1(GraphicsContext gc, Vertex so, Vertex ta) {
 
 		gc.setStroke(Color.BLUE);
