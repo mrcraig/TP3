d11e04a41f00f73ca13eb966a16661d70484eb16
mrcraig <1002386c@student.gla.ac.uk>
2013-03-03 15:32:15 +0000

    Table working fully!

--- src/thesaurus/gui/table/ViewTable.java
+++ src/thesaurus/gui/table/ViewTable.java
@@ -1,5 +1,9 @@
 package thesaurus.gui.table;
 
+import java.util.LinkedList;
+
+import com.sun.net.ssl.SSLContext;
+
 import thesaurus.gui.window.VisualisationRoot;
 import thesaurus.parser.Vertex;
 import javafx.beans.property.SimpleStringProperty;
@@ -66,7 +70,218 @@ public class ViewTable {
     	return table;
     }
     
+    private boolean arrayContains(LinkedList<String> arr,String con){
+    	for(String s:arr){
+    		if(s.compareTo(con)==0)
+    			return true;
+    	}
+    	
+    	return false;
+    }
+    
     private void importData(){
+    	LinkedList<String> rootWords = new LinkedList<String>();
+		String synList="";
+		String antList="";
+		
+    	if(!arrayContains(rootWords,vertex.getWord())){
+    		
+    		for(int i=0;i<vertex.getSynomyns().size();i++){
+        		if(windowWidth>400){
+    	    		if(i%4==0 && i>0){
+    	    			synList += "\n";
+    	    		} else if(i>0){
+    	    			synList += ", ";
+    	    		}
+        		} else {
+        			if(i>0){
+        				synList += "\n";
+        			}
+        		}
+        		synList += vertex.getSynomyns().get(i).getWord();
+        	}
+        	
+        	for(int i=0;i<vertex.getAntonyms().size();i++){
+        		if(windowWidth>400){
+    	    		if(i%4==0 && i>0){
+    	    			antList += "\n";
+    	    		} else if(i>0){
+    	    			antList += ", ";
+    	    		}
+        		} else {
+        			if(i>0){
+        				antList += "\n";
+        			}
+        		}
+        		antList += vertex.getSynomyns().get(i).getWord();
+        	}
+    	}
+    	
+    	//Add data to table
+    	data.add(new TabData(vertex.getWord(),synList,antList));
+    	rootWords.add(vertex.getWord());
+    	
+    	for(Vertex v: vertex.getSynomyns()){
+    		if(!arrayContains(rootWords,v.getWord())){
+	    		synList="";
+	    		antList="";
+	    		
+	    		for(int i=0;i<v.getSynomyns().size();i++){
+	    			if(windowWidth>400){
+	    				if(i%4==0 && i>0){
+	    					synList += "\n";
+	    				} else if(i>0){
+	    					synList+=", ";
+	    				}
+	    			} else {
+	    				if(i>0){
+	    					synList += "\n";
+	    				}
+	    			}
+	    			synList+=v.getSynomyns().get(i).getWord();
+	    		}
+	    		
+	    		for(int i=0;i<v.getAntonyms().size();i++){
+	    			if(windowWidth>400){
+	    				if(i%4==0 && i>0){
+	    					antList += "\n";
+	    				} else if(i>0){
+	    					antList+=", ";
+	    				}
+	    			} else {
+	    				if(i>0){
+	    					antList += "\n";
+	    				}
+	    			}
+	    			antList+=v.getAntonyms().get(i).getWord();
+	    		}
+	    		data.add(new TabData(v.getWord(),synList,antList));
+	    		rootWords.add(v.getWord());
+	    		
+	    		for(Vertex n:v.getSynomyns()){
+	    			if(!arrayContains(rootWords,n.getWord())){
+	    				synList="";
+	    	    		antList="";
+	    	    		
+	    	    		for(int i=0;i<n.getSynomyns().size();i++){
+	    	    			if(windowWidth>400){
+	    	    				if(i%4==0 && i>0){
+	    	    					synList += "\n";
+	    	    				} else if(i>0){
+	    	    					synList+=", ";
+	    	    				}
+	    	    			} else {
+	    	    				if(i>0){
+	    	    					synList += "\n";
+	    	    				}
+	    	    			}
+	    	    			synList+=n.getSynomyns().get(i).getWord();
+	    	    		}
+	    	    		
+	    	    		for(int i=0;i<n.getAntonyms().size();i++){
+	    	    			if(windowWidth>400){
+	    	    				if(i%4==0 && i>0){
+	    	    					antList += "\n";
+	    	    				} else if(i>0){
+	    	    					antList+=", ";
+	    	    				}
+	    	    			} else {
+	    	    				if(i>0){
+	    	    					antList += "\n";
+	    	    				}
+	    	    			}
+	    	    			antList+=n.getAntonyms().get(i).getWord();
+	    	    		}
+	    	    		//Add data to table
+	    	    		data.add(new TabData(n.getWord(),synList,antList));
+	    	    		rootWords.add(n.getWord());
+	    			}
+	    		}
+    		}
+    	}
+    	
+    	
+    	/* ANTONYMS */
+    	
+    	for(Vertex v: vertex.getAntonyms()){
+    		if(!arrayContains(rootWords,v.getWord())){
+	    		synList="";
+	    		antList="";
+	    		
+	    		for(int i=0;i<v.getSynomyns().size();i++){
+	    			if(windowWidth>400){
+	    				if(i%4==0 && i>0){
+	    					synList += "\n";
+	    				} else if(i>0){
+	    					synList+=", ";
+	    				}
+	    			} else {
+	    				if(i>0){
+	    					synList += "\n";
+	    				}
+	    			}
+	    			synList+=v.getSynomyns().get(i).getWord();
+	    		}
+	    		
+	    		for(int i=0;i<v.getAntonyms().size();i++){
+	    			if(windowWidth>400){
+	    				if(i%4==0 && i>0){
+	    					antList += "\n";
+	    				} else if(i>0){
+	    					antList+=", ";
+	    				}
+	    			} else {
+	    				if(i>0){
+	    					antList += "\n";
+	    				}
+	    			}
+	    			antList+=v.getAntonyms().get(i).getWord();
+	    		}
+	    		data.add(new TabData(v.getWord(),synList,antList));
+	    		rootWords.add(v.getWord());
+	    		
+	    		for(Vertex n:v.getSynomyns()){
+	    			if(!arrayContains(rootWords,n.getWord())){
+	    				synList="";
+	    	    		antList="";
+	    	    		
+	    	    		for(int i=0;i<n.getSynomyns().size();i++){
+	    	    			if(windowWidth>400){
+	    	    				if(i%4==0 && i>0){
+	    	    					synList += "\n";
+	    	    				} else if(i>0){
+	    	    					synList+=", ";
+	    	    				}
+	    	    			} else {
+	    	    				if(i>0){
+	    	    					synList += "\n";
+	    	    				}
+	    	    			}
+	    	    			synList+=n.getSynomyns().get(i).getWord();
+	    	    		}
+	    	    		
+	    	    		for(int i=0;i<n.getAntonyms().size();i++){
+	    	    			if(windowWidth>400){
+	    	    				if(i%4==0 && i>0){
+	    	    					antList += "\n";
+	    	    				} else if(i>0){
+	    	    					antList+=", ";
+	    	    				}
+	    	    			} else {
+	    	    				if(i>0){
+	    	    					antList += "\n";
+	    	    				}
+	    	    			}
+	    	    			antList+=n.getAntonyms().get(i).getWord();
+	    	    		}
+	    	    		//Add data to table
+	    	    		data.add(new TabData(n.getWord(),synList,antList));
+	    	    		rootWords.add(n.getWord());
+	    			}
+	    		}
+    		}
+    	}
+    	
     	/* Main Vertex 
     	String synList = "";
     	String antList = "";
