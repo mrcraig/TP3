86eeecb1865906327d73c3ee47491de435e14d86
1000011c@student.gla.ac.uk <1000011c@student.gla.ac.uk>
2013-03-03 15:19:08 +0000

    Ready to use text parser

--- src/thesaurus/importparser/TextParser.java
+++ src/thesaurus/importparser/TextParser.java
@@ -2,41 +2,50 @@ package thesaurus.importparser;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class TextParser {
 
-
-public static void main(String[] args)
-{ 
-	File f = new File("/users/level3/0805089k/massivechore.txt");
-	Scanner fileScan = null;
-	try {
-		fileScan = new Scanner(f);
-		fileScan.useDelimiter("\n");
-	} catch (FileNotFoundException e) {
-		e.printStackTrace();
-	}
-	List allTheWords = new LinkedList();
-	List wordsWordsWords = null;
-	while(fileScan.hasNext()){
-			String words = fileScan.next();
-			
-			if(!words.isEmpty())
-			{
-				if(words.split(" ").length==1)
-				{
-					allTheWords.add(wordsWordsWords);
-					wordsWordsWords = new LinkedList();
-				}
-				words = words.replace(",","");
-				wordsWordsWords.add(words);
-				System.out.println();
-				System.out.println(wordsWordsWords);
-			}		
+	public static void main(String[] args) {
+		File recentFile = new File("./src/resourcePackage/importparser.txt");
+		Scanner fileScan = null;
+		try {
+			fileScan = new Scanner(recentFile);
+			fileScan.useDelimiter("\n");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
-			
+		ArrayList<ArrayList<String>> entryArray = new ArrayList<ArrayList<String>>();
+		int index = 0;
+		int entryindex = 0;
+		while (fileScan.hasNext()) {
+			String words = fileScan.next();
+			if (!words.isEmpty()) {
+				if (index == 0) {
+					ArrayList<String> currentEntry = new ArrayList<String>();
+					entryArray.add(entryindex, currentEntry);
+					currentEntry.add(0, words);
+					index++;
+				} else {
+					entryArray.get(entryindex).add(index, words);
+					index++;
+				}
+			} else {
+				index = 0;
+				entryindex++;
+			}
+
+
+		}
+
+		for (ArrayList<String> s : entryArray) {
+			for (String st : s) {
+				System.out.println(st);
+			}
+		}
+
 	}
 }
