78093f6cab0ad80e81cd535c74dcdfc81c58c52b
James <0805089k@student.gla.ac.uk>
2013-03-10 15:18:27 +0000

    remove finally, finally, done

--- src/thesaurus/parser/HashGraph.java
+++ src/thesaurus/parser/HashGraph.java
@@ -2,130 +2,150 @@ package thesaurus.parser;
 
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.Map.Entry;
 import java.util.Set;
-
 
 //use string as index
 
 //use word first then maybe index
-public class HashGraph
-{
-	private HashMap<String,Vertex> nodesMap;
-	
+public class HashGraph {
+	private HashMap<String, Vertex> nodesMap;
 
-
-
-	public HashGraph()
-	{
+	public HashGraph() {
 		nodesMap = new HashMap<String, Vertex>();
-		
+
 	}
 
-	void add(Vertex v)
-	{
+	void add(Vertex v) {
 		nodesMap.put(v.getWord(), v);
 	}
 
-	int size()
-	{
+	int size() {
 		return nodesMap.size();
 	}
 
-	Vertex getVertexFromIndex(String index)
-	{
-		Iterator<Entry<String,Vertex>> all = nodesMap.entrySet().iterator();
-		while(all.hasNext())
-		{
+	Vertex getVertexFromIndex(String index) {
+		Iterator<Entry<String, Vertex>> all = nodesMap.entrySet().iterator();
+		while (all.hasNext()) {
 			Vertex v = all.next().getValue();
-			if(v.getID().equals(index))
-			{
+			if (v.getID().equals(index)) {
 				return v;
 			}
 		}
 		System.out.println("null");
 		return null;
 	}
-	
-	void setNodes(HashMap<String,Vertex> nodes)
-	{
+
+	void setNodes(HashMap<String, Vertex> nodes) {
 		this.nodesMap = nodes;
 	}
-	
-	Vertex getVertexFromWord(String word)
-	{
+
+	Vertex getVertexFromWord(String word) {
 		return nodesMap.get(word);
 	}
-	
-	Vertex getRandVertex()
-	{
+
+	Vertex getRandVertex() {
 		return nodesMap.entrySet().iterator().next().getValue();
-		
+
 	}
 
-	boolean contains(Vertex v) 
-	{
+	boolean contains(Vertex v) {
 		return nodesMap.containsValue(v);
 	}
 
-
-    HashMap<String,Vertex> getNodes()
-	{
+	HashMap<String, Vertex> getNodes() {
 		return nodesMap;
 	}
 
-    //
-    //remove key and value
-    void removeVertex(Vertex old)
-    {
-    	String word = old.getWord();
-    	System.out.println("word to be deleted "+word);
-    	nodesMap.remove(word);
-    	Iterator<Entry<String,Vertex>> all = nodesMap.entrySet().iterator();
-    	System.out.println("nodes at start " + nodesMap);
-    	while(all.hasNext())
-    	{
-    		Vertex v = all.next().getValue();
-    		System.out.println("currently checking "+v.getWord());
-    		Iterator<Vertex> antonyms = v.getAntonyms().iterator();
-    		while(antonyms.hasNext())
-			 {
-    			 Vertex a = antonyms.next();
-				 if(a.getWord().equals(word))
-					 {
-					 	System.out.println("removing antonym "+word);
-					 	//v.removeAntonym(a);
-					 	nodesMap.remove(a.getWord());
-					 }
-			 }
+	void removeVertex(Vertex old) {
+		String word = old.getWord();
+		System.out.println("word to be deleted " + word);
+		nodesMap.remove(word);
+		Iterator<Entry<String, Vertex>> all = nodesMap.entrySet().iterator();
+		LinkedList<RemoveEntry> verticesRemove = new LinkedList<RemoveEntry>();
 
-    		Iterator<Vertex> synonyms = v.getSynomyns().iterator();
-			 while(synonyms.hasNext())
-			 {
-				 Vertex s = synonyms.next();
-				 if(s.getWord().equals(word))
-					 {
-					
-					 System.out.println("removing synonym "+word);
-					 	//v.removeSynonym(s);
-					 	nodesMap.remove(s.getWord());
-					 }
-			 }
-			 
-			 Iterator<Vertex> groupings = v.getGroupings().iterator();
-			 while(groupings.hasNext())
-			 {
-				 Vertex g = groupings.next();
-				 if(g.getWord().equals(word))
-				 {
-					 System.out.println("removing grouping "+word);
-					 //v.removeGrouping(g);
-					 nodesMap.remove(g.getWord());
-				 }
-			 }
-    	}
-    	System.out.println("nodes at end " + nodesMap);
-    }
+		while (all.hasNext()) {
+			Vertex v = all.next().getValue();
+			System.out.println("currently checking " + v.getWord());
+			Iterator<Vertex> antonyms = v.getAntonyms().iterator();
+			RemoveEntry r = new RemoveEntry();
+			r.v = v;
+			while (antonyms.hasNext()) {
+				Vertex a = antonyms.next();
+				if (a.getWord().equals(word)) {
+					System.out.println("removing antonym " + word);
+					r.la.add(a);
+					// nodesMap.remove(a.getWord());
+				}
+			}
 
-    
+			Iterator<Vertex> synonyms = v.getSynomyns().iterator();
+			while (synonyms.hasNext()) {
+				Vertex s = synonyms.next();
+				if (s.getWord().equals(word)) {
+
+					System.out.println("removing synonym " + word);
+					r.ls.add(s);
+					// v.removeSynonym(s);
+					// nodesMap.remove(s.getWord());
+				}
+			}
+
+			Iterator<Vertex> groupings = v.getGroupings().iterator();
+			while (groupings.hasNext()) {
+				Vertex g = groupings.next();
+				if (g.getWord().equals(word)) {
+					System.out.println("removing grouping " + word);
+					// v.removeGrouping(g);
+					r.lg.add(g);
+					// nodesMap.remove(g.getWord());
+				}
+			}
+			
+			verticesRemove.add(r);
+			
+		}
+		
+		removeVertices(verticesRemove);
+		
+	}
+	
+	void removeVertices(LinkedList<RemoveEntry> entries)
+	{
+		for(RemoveEntry e : entries)
+		{
+			for(Vertex va: e.la){
+				e.v.removeAntonym(va);
+			}
+			
+			for(Vertex vs: e.ls){
+				e.v.removeSynonym(vs);
+			}
+			
+			for(Vertex vg: e.lg){
+				e.v.removeGrouping(vg);
+			}
+			
+		}
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
